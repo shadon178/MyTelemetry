29412b4fe0635f20500283aca01c90a4e3376475
pxd <pxd@willfine.com>
2025-07-25 17:52:58 +0800

    init

---
+++ src/main/java/com/pxd/telemetry/OpenTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.pxd.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTelemetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenTelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pxd/telemetry/OpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxd.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
