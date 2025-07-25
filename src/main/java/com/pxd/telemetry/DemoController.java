e79b5f7e6d826c366468702dbf9648eb4378f00d
pxd <pxd@willfine.com>
2025-07-25 18:35:14 +0800

    init

---
+++ src/main/java/com/pxd/telemetry/DemoController.java
@@ -0,0 +1,21 @@
+package com.pxd.telemetry;
+
+import com.pxd.telemetry.entity.DemoResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public DemoResponse hello() {
+        DemoResponse demoResponse = new DemoResponse();
+        demoResponse.setName("Hello World");
+        demoResponse.setAge(100);
+        log.info("hello: response = {}", demoResponse);
+        return demoResponse;
+    }
+
+}
---
+++ src/main/java/com/pxd/telemetry/entity/DemoResponse.java
@@ -0,0 +1,12 @@
+package com.pxd.telemetry.entity;
+
+import lombok.Data;
+
+@Data
+public class DemoResponse {
+
+    private String name;
+
+    private Integer age;
+
+}
