d4a3bcd37733b95fea8372bba26848f61a00c105
pxd <pxd@willfine.com>
2025-07-31 14:15:36 +0800

    init

--- src/main/java/com/pxd/telemetry/OpenTelemetryApplication.java
+++ src/main/java/com/pxd/telemetry/OpenTelemetryApplication.java
@@ -1,6 +1,5 @@
 package com.pxd.telemetry;
 
-import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenTelemetryApplication {
 
     public static void main(String[] args) {
-        ElasticApmAttacher.attach();
         SpringApplication.run(OpenTelemetryApplication.class, args);
     }
 
